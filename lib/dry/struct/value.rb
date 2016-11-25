68e10210c44f9130d4084b4e18939a7823b8510e
Alex Semyonov <alex@semyonov.us>
2016-11-26 01:50:48 +0300

    Document Dry::Struct::Value

--- lib/dry/struct/value.rb
+++ lib/dry/struct/value.rb
@@ -2,7 +2,26 @@ require 'ice_nine'
 
 module Dry
   class Struct
+    # {Value} objects behave like {Struct}s but *deeply frozen*
+    # using [`ice_nine`](https://github.com/dkubb/ice_nine)
+    #
+    # @example
+    #   class Location < Dry::Struct::Value
+    #     attribute :lat, Types::Strict::Float
+    #     attribute :lng, Types::Strict::Float
+    #   end
+    #
+    #   loc1 = Location.new(lat: 1.23, lng: 4.56)
+    #   loc2 = Location.new(lat: 1.23, lng: 4.56)
+    #
+    #   loc1.frozen? #=> true
+    #   loc2.frozen? #=> true
+    #   loc1 == loc2 #=> true
+    #
+    # @see https://github.com/dkubb/ice_nine
     class Value < self
+      # @see ClassInterface#new
+      # @see https://github.com/dkubb/ice_nine
       def self.new(*)
         IceNine.deep_freeze(super)
       end
