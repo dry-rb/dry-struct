87879d866fd302b3f4326085130fa39aec170512
Alex Semyonov <alex@semyonov.us>
2016-11-26 01:50:00 +0300

    Document exceptions

--- lib/dry/struct/errors.rb
+++ lib/dry/struct/errors.rb
@@ -4,9 +4,13 @@ module Dry
 
     setting :namespace, self
 
+    # Raised when given input doesn't conform schema and constructor type
     Error = Class.new(TypeError)
 
+    # Raised when defining duplicate attributes
     class RepeatedAttributeError < ArgumentError
+      # @param [Symbol] key
+      #   attribute name that is the same as previously defined one
       def initialize(key)
         super("Attribute :#{key} has already been defined")
       end
