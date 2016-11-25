d196a3343bfa7905657bc844988e85a1a83875a8
Alex Semyonov <alex@semyonov.us>
2016-11-26 01:50:38 +0300

    Document Dry::Struct::Hashify

--- lib/dry/struct/hashify.rb
+++ lib/dry/struct/hashify.rb
@@ -1,8 +1,10 @@
-# Converts value to hash recursively
-
 module Dry
   class Struct
+    # Helper for {Struct#to_hash} implementation
     module Hashify
+      # Converts value to hash recursively
+      # @param [#to_hash, #map, Object] value
+      # @return [Hash, Array]
       def self.[](value)
         if value.respond_to?(:to_hash)
           value.to_hash
