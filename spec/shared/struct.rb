f612b526930e399556b22e379ce483b9654ac56c
Nikita Shilnikov <fg@flashgordon.ru>
2019-02-23 22:20:33 +0300

    Type transformation block now yields 1 arg

--- spec/shared/struct.rb
+++ spec/shared/struct.rb
@@ -191,7 +191,7 @@ RSpec.shared_examples_for Dry::Struct do
       end
 
       it 'accepts a proc' do
-        type.transform_types(-> (t, _name) { t.meta(tranformed: true) })
+        type.transform_types(-> (key) { key.meta(tranformed: true) })
         type.attribute(:city, Dry::Types["strict.string"])
         expect(type.schema.key(:city).type.meta).to eql(tranformed: true)
       end
